71185c0b44653ed973217fedd565ecc335ce9b9f
Minh <minhncq99@gmail.com>
2020-05-13 16:13:58 +0700

    Add Update Disease

--- LTCSDL.DAL/DiseasesRep.cs
+++ LTCSDL.DAL/DiseasesRep.cs
@@ -1,6 +1,8 @@
 ﻿using LTCSDL.Common.DAL;
 using System.Linq;
 using LTCSDL.DAL.Models;
+using LTCSDL.Common.Rsp;
+using System;
 
 namespace LTCSDL.DAL
 {
@@ -18,6 +20,63 @@ namespace LTCSDL.DAL
             var m = base.All.First(p => p.DiseaseId == id);
             m = base.Delete(m);
             return m.DiseaseId;
+        }
+
+        #endregion
+
+        #region
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public SingleRsp CreateDisease(Disease disease)
+        {
+            var res = new SingleRsp();
+
+            using (var context = new EhealthContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Diseases.Add(disease);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        res.SetError(ex.StackTrace);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public SingleRsp UpdateDisease(Disease disease)
+        {
+            var res = new SingleRsp();
+
+            using (var context = new EhealthContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Diseases.Update(disease);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        res.SetError(ex.StackTrace);
+                    }
+                }
+            }
+
+            return res;
         }
 
         #endregion
